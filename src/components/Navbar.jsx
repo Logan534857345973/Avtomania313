f092be1b85ee6bd4155d62332acea1f78d5cea2d
Logan534857345973 <Loganenemy@gmail.com>
2024-06-14 15:06:40 +0300

    Avtomaniaplus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
-import SearchIcon from '@mui/icons-material/Search';
-import {BadgeIcon} from '@mui/icons-material/Badge';
-import {ShoppingCartIcon} from '@mui/icons-material/ShoppingCart';
+import SearchIcon from "@mui/icons-material/Search";
+// import {BadgeIcon} from '@mui/icons-material/Badge';
+import { ShoppingCart } from '@mui/icons-material';
 
 // Картинки
 import Car from "../components/img/Car.jpg"
-import Car1 from "../components/img/Car1.jpg"
 import Car3 from "../components/img/Car3.jpg"
 import Car4 from "../components/img/Car4.jpg"
+import { Badge } from '@mui/material';
 
 const Container = styled.div`
     height: 60px;
-    background-color: #ffffff;
+    background-color: #ffffff; 
 `
 
 
@@ -50,93 +50,114 @@ const SearchContainer = styled.div`
 const Input = styled.input`
     border: none;
 `
-const Logo = styled.h1`
+const Logo = styled.div`
     font-weight: bold;
 `
+
 
 const MenuItem = styled.div`
      font-size: 14px;
     cursor: pointer;
     margin-right: 30px;
 `
-const ImgContainer = styled.div`
-    & img{
-        width: 350px;
-    }
-`
-
-const ImgContainer1 = styled.div`
-    & img{
-        width: 600px;
-        left: 10px;
-        margin-right: 10px;
-    }
-`
 
 const Iimages = styled.div`
     display:flex;
     display: block;
     margin-right: 50px;
-`
-const Iimages1 = styled.div`
-    display:flex;
-    display: block;
-    margin-right: 50px;
+  
 `
 
-const ImgContainerr = styled.div`
+const Main = styled.div`
     & img{
         width: 600px;
         left: 10px;
-        /* margin-left: 1005px; */
+         margin-left: 100px;  
+         margin-top: 100px;
         display: flex;
     }
 `
 
+const Iimages1 = styled.div`
+    & img{
+        width: 600px;
+        left: 10px;
+        margin-left: 1000px;
+        margin-top: -1185px;
+        display: flex;
+    }
+`
+
+const Text = styled.div`
+        margin-left: 1000px;
+`
+const Text2 = styled.div`
+        margin-left: 100px;
+`
+
+const Text3 = styled.div`
+        margin-left: 100px;
+`
+
 const Navbar = () => {
-  return (
-<ImgContainer>
-    <ImgContainer1>
-    <Container>
-        <Wrapper>
-            <Left>
-                <SearchContainer>
-                    <SearchIcon style={{color:"gray", fontSize:16}}/>
-                    <Input></Input>
-                </SearchContainer>
-            </Left>
-                
-                    <Logo>AVTONOMIA</Logo>
+    return (
+        <Main>
+            <Container>
+                <Wrapper>
+                    <Left>
+                        <SearchContainer>
+                            <SearchIcon style={{ color: "gray", fontSize: 16 }} />
+                            <Input></Input>
+                        </SearchContainer>
+                    </Left>
+                    <Conter>
+                        <Logo>AVTONOMIA</Logo>
+                    </Conter>
+                    <Right>
+                        <form action="Registration.js">
+                        <button>Регистрация</button>
+                        </form> 
+                        <form action="">
+                        <button>Войти</button>
+                        </form>    
+                        <form action="">
+                        <button>Контакты</button>
+                        </form>     
+                        <MenuItem>
+                            <Badge badgeContent={4} color='primary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </MenuItem>
+                    </Right>
+                </Wrapper>
+            </Container>
+
+            <Iimages>
+        <img src={Car} alt="Car"/>
+        <Text3>
+        <h1>Машина Mercedes-Benz S-Класс</h1>
+        <h2>Машина S-Класса, довольно крупная для мерседеса как и по длине, 330км/ч</h2>
+        <h3>Цена: ~18.000.000 рублей</h3>
+        </Text3>
+        <img src={Car3} alt="Car3"/>
+        <Text2>
+        <h1>Машина BMW</h1>
+        <h2>Машина BMW очень быстрая и разгон 340 км/ч</h2>
+        <h3>Цена: ~5.500.000 рублей </h3>
+        </Text2>
+       </Iimages>  
+       <Iimages1>
+        <img src={Car4} alt="Car4"/>
+        <Text>
+        <h1>Машина Lamborghini Hurakan</h1>
+        <h2>Машина Lamborghini Hurakan очень быстрая и разгон 320 км/ч</h2>
+        <h3>Цена: ~20.000.000 рублей </h3>
+        </Text>
+        </Iimages1>
+        </Main>
 
    
-
-                    <Conter>
-                </Conter>
-            <Right>
-                <MenuItem>Регистрация</MenuItem>
-                <MenuItem>Войти</MenuItem>
-                <MenuItem>
-                    <BadgeIcon badgeContent={4} color='primary'>
-                        <ShoppingCartIcon></ShoppingCartIcon>
-                    </BadgeIcon>
-                </MenuItem>
-            </Right>
-        </Wrapper>
-        <Iimages>
-        <img src={Car} alt="Car"/>
-        <img src={Car3} alt="Car3"/>
-        </Iimages>
-        <Iimages>
-        <img src={Car1} alt="Car1"/>
-        <img src={Car4} alt="Car4"/>
-        </Iimages>
-        <ImgContainerr>
-        <Iimages1></Iimages1>
-        </ImgContainerr>
-    </Container>
-    </ImgContainer1>
-</ImgContainer>
-  )
+    )
 }
 
 
---
+++ src/pages/Registration page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Fon from "../components/img/Fon.jpg"
+import { Container } from '@mui/material'
+const Registration  = () => {
+  return (
+    <div>
+   <Container>
+    <Images>
+    <img src={Fon} alt="Fon"/>
+    </Images>
+   </Container>
+    </div>
+  )
+}
+
+export default Registration 
